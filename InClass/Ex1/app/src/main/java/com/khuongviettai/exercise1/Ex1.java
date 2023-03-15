0304fc1f3b1550f04461e30193793e60dff9d488
khuongviettai <khuongviettai@outlook.com>
2023-03-15 13:54:52 +0700

    first commit

---
+++ InClass/Ex1/app/src/main/java/com/khuongviettai/exercise1/Ex1.java
@@ -0,0 +1,14 @@
+package com.khuongviettai.exercise1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ex1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex1);
+    }
+}
