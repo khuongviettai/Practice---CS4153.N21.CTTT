1f56fea7b52a04a2f3fdacc76a2ad877d39225a5
Khương Viết Tài <khuongtai@MacBook-Pro.local>
2023-04-12 14:34:29 +0700

    update lab3

---
+++ InClass/Ex3/Ex33/app/src/main/java/com/khuongviettai/ex33/MainActivity.java
@@ -0,0 +1,14 @@
+package com.khuongviettai.ex33;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new GraphicView(this));
+    }
+}
